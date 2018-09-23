3a47be938667b1244aa729d4ff6a67cca92c72f2
Jeff Goldsmith <jeff@JeffCicis-Mini.local>
2018-09-23 12:32:49 -0400

    add documentation

--- R/datasets.R
+++ R/datasets.R
@@ -1,8 +1,36 @@
 # Documentation for datasets
 
-#' @name blank
+#' @name instacart
 #' @docType data
-#' @title blank
-#' @description blank
+#' @title Instacart
+#' @description The instacart dataset; see course website
+#' for deails. 
+#' @keywords data
+NULL
+
+
+#' @name ny_noaa
+#' @docType data
+#' @title NY State NOAA
+#' @description The ny_noaa dataset; see course website
+#' for deails. 
+#' @keywords data
+NULL
+
+
+#' @name nyc_airbnb
+#' @docType data
+#' @title NYC Airbnb data
+#' @description The nyc_airbnb dataset; see course website
+#' for deails. 
+#' @keywords data
+NULL
+
+
+#' @name rest_inspec
+#' @docType data
+#' @title NYC Restaurant Inspection data
+#' @description The rest_inspec dataset; see course website
+#' for deails. 
 #' @keywords data
 NULL
