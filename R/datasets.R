ef85f5974560ca8095bf68cb93b07c0b35c0af58
Jeff Goldsmith <jeff@JeffCicis-Mini.local>
2018-09-23 11:57:28 -0400

    initial commit; package infrastructure

---
+++ R/datasets.R
@@ -0,0 +1,8 @@
+# Documentation for tidyfun datasets
+
+#' @name blank
+#' @docType data
+#' @title blank
+#' @description blank
+#' @keywords data
+NULL
---
+++ R/p8105.datasets-package.r
@@ -0,0 +1,14 @@
+#' p8105.datasets
+#'
+#' `p8105.datasets` is an R package containing datasets for P8105.
+#'
+#' The package includes
+#'
+#'   - AirBnB data
+#'   - Instacart data
+#'   - NOAA data
+#'   - NYC Restaurant Inspection data
+#'
+#' @name tidyfun
+#' @docType package
+NULL
